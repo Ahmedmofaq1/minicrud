4b551bbcd69453209898a233eadf552ee3efd966
Ahmedmofaq1 <94381478+Ahmedmofaq1@users.noreply.github.com>
2022-03-14 15:55:52 +0100

    initial commit

---
+++ About/About.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <h2>ahhj</h2>
+    <?<php
+    
+echo "hello world";
+    
+    ?>
+
+    
+    <div class="frame">
+
+	<div class="menus">
+
+		<a href="Home.php" class="menu active">Home</a>
+		<a href="service.php" class="menu">Service</a>
+		<a href="About.php" class="menu">About</a>
+
+	</div>
+
+	<div class="title-lead">
+
+	
+</div>
+</body>
+</html>
