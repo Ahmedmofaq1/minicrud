f64d95ac13b954b731b00b9d0b7bcbb3e9e8c445
Ahmedmofaq1 <94381478+Ahmedmofaq1@users.noreply.github.com>
2022-03-21 15:47:57 +0100

    initial commit

--- index.php
+++ index.php
@@ -117,20 +117,20 @@ try {
       
         <?php
 
-$data = $conn->query("SELECT * FROM album")->fetchAll();
+$data = $conn->query("SELECT * FROM cafetaria")->fetchAll();
 // and somewhere later:
 foreach ($data as $row) {
     ?>
 
         <div class="col">
           <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+           <img src="./img/<?php echo $row['foto'];?>" alt="">
 
             <div class="card-body">
               <p class="card-text"><?php echo $row['titel'];?> prijs: <?php echo $row['prijs'];?>; op voorraad: <?php echo $row['voorraad'];?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <a href="product.php?p=<?php echo $row['ID'];?>" class="btn btn-sm btn-outline-secondary">View</a>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
                 <small class="text-muted">9 mins</small>
